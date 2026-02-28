2c7ae9228e2b7702865a511f7edc136f03f5ed99
Mubasher Khattak <mubasherkhattak92@outlook.com>
2026-02-28 06:24:35 +0500

    Add react-native-gesture-handler import

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler'; // MUST BE FIRST
 import React, { useEffect } from 'react';
 import { StatusBar, useColorScheme } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
